c4083df1bce4f7968cb6f42e16ee6f015bdff881
Mrbabet <108229726+Mrbabet@users.noreply.github.com>
2022-08-24 20:55:22 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const topHeaderWrapper = document.querySelector(".top-header-wrapper");
 const checkbox = document.querySelector(".checkbox");
 const gridBoxTotal = document.querySelectorAll(".grid-box-total");
 const gridBoxToday = document.querySelectorAll(".grid-box-today");
+const newTextDocument = document.querySelector(".Dark-mode-text");
 
 checkbox.addEventListener("change", () => {
   gridBoxTotal.forEach((gridBoxTotal) => {
@@ -20,4 +21,18 @@ checkbox.addEventListener("change", () => {
   body.classList.toggle("dark");
   topHeader.classList.toggle("dark");
   topHeaderWrapper.classList.toggle("dark");
+});
+
+checkbox.addEventListener("click", function handleClick() {
+  const initialText = "Dark mode";
+
+  if (
+    newTextDocument.textContent
+      .toLowerCase()
+      .includes(initialText.toLowerCase())
+  ) {
+    newTextDocument.textContent = "Light mode";
+  } else {
+    newTextDocument.textContent = initialText;
+  }
 });
